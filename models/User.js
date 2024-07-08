60ac10a2830f2d557c052911714638220a06b7c8
Robb753 <122726102+Robb753@users.noreply.github.com>
2024-07-08 21:22:16 +0200

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  produce: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
