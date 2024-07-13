69bdc7f003d385d9a8433611f401e70a23b4bcc6
Robb753 <122726102+Robb753@users.noreply.github.com>
2024-07-13 10:29:57 +0200

    maj

--- src/Components/MapComponent.jsx
+++ src/Components/MapComponent.jsx
@@ -12,7 +12,7 @@ const MapComponent = () => {
   const markersRef = useRef([]);
 
   useEffect(() => {
-    const apiUrl = process.env.REACT_APP_API_URL,
+    const apiUrl = ("${process.env.REACT_APP_API_URL}/GetUsers");
     fetch(apiUrl).then((response) => {
       if (!response.ok) {
         throw new Error('HTTP error! Status: ${response.status}');
